949750c02a12b56bae095744f2531a9af85956e3
Aditya Rajput <131146808+aditya11214@users.noreply.github.com>
2023-10-25 18:48:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
 
+// Array of band names
+let bandNames = ['The Rolling Stones', 'Aerosmith', 'The Beatles', 'Led Zeppelin', 'Guns N Roses'];
+
+// Function to remove articles from band names
+function removeArticles(name) {
+  return name.replace(/^(?:the|a|an) /i, '');
+}
+
+// Remove articles and sort the band names
+const sortedBandNames = bandNames.map(removeArticles).sort();
+
+// Create an HTML list
+const ul = document.getElementById('band');
+
+sortedBandNames.forEach((name) => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  ul.appendChild(li);
+});
